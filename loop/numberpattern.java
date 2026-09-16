8246d8c3da9ccf9400d818b6d7fd5052df825c4f
thatshekhar <thatshekhar@shekhargangwar25>
2026-09-16 18:18:10 +0530

    ..

---
+++ loop/numberpattern.java
@@ -0,0 +1,21 @@
+package loop;
+
+
+import java.util.Scanner;
+
+class numberpattern{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int num = 1;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+        
+    }
+}
